17a0e4410e490812b7076ba5952c4db4770b3025
Marco Herrero <me@marhs.de>
2019-01-11 10:44:21 +0100

    Add input source

--- 1/day1a.py
+++ 1/day1a.py
@@ -1,2 +1,9 @@
+input = open('input.txt', 'r').read()
+
+
 def separate_steps(string):
     return sum([int(x) for x in string.split()])
+
+
+if __name__ == "__main__":
+    print(separate_steps(input))
